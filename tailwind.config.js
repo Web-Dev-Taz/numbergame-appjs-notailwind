d1935fa72a105ccc7642eef6a400e4e45ac5ac53
Taz <47963508+Web-Dev-Taz@users.noreply.github.com>
2022-11-01 23:40:43 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ["./*.html"],
+   theme: {
+      screens: {
+         sm: "480px",
+         md: "768px",
+         lg: "976px",
+         xl: "1440px",
+      },
+      extend: {
+         colors: {
+            teal: "#2b6777",
+            gray: "#c8d8e4",
+            brown: "#D7C0AE",
+            green: "#52ab98",
+            white: "#E8F9FD",
+         },
+      },
+   },
+   plugins: [],
+}
